2b7aff64806b3d4186e6d9bb0aea9e3888919a84
Chloe Schaff <47031895+nonbinarysolo@users.noreply.github.com>
2023-06-11 14:43:18 -0400

    Set up XY sensorless homing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1708,10 +1708,10 @@
 #define Y_BED_SIZE 200
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define X_MIN_POS -30
+#define Y_MIN_POS -15
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
+#define X_MAX_POS 210
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 200
 //#define I_MIN_POS 0
